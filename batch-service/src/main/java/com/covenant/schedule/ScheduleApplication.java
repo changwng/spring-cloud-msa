6c79bdd779b892cb8cb142bf4f53caeed0e6410f
jin <snf01003@ex-em.com>
2024-03-28 14:25:55 +0900

    [configsettings] local환경, docker-compose dev & k8s prod 개발환경에서 구동될 수 있게 application 설정 파일 분리

--- batch-service/src/main/java/com/covenant/schedule/ScheduleApplication.java
+++ batch-service/src/main/java/com/covenant/schedule/ScheduleApplication.java
@@ -3,9 +3,11 @@ package com.covenant.schedule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ScheduleApplication {
 
     public static void main(String[] args) {
